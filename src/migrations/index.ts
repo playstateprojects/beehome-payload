38d1fea8f847ad308bf4d057620de23d9a547245
Tom <tom@playstate.co.za>
2025-10-10 10:58:24 +0200

    payload in app notifications

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,21 +1,9 @@
-import * as migration_20250929_111647 from './20250929_111647';
-import * as migration_20251010_041607 from './20251010_041607';
-import * as migration_20251010_071607 from './20251010_071607';
+import * as migration_20251010_085528_initial from './20251010_085528_initial';
 
 export const migrations = [
   {
-    up: migration_20250929_111647.up,
-    down: migration_20250929_111647.down,
-    name: '20250929_111647',
-  },
-  {
-    up: migration_20251010_041607.up,
-    down: migration_20251010_041607.down,
-    name: '20251010_041607',
-  },
-  {
-    up: migration_20251010_071607.up,
-    down: migration_20251010_071607.down,
-    name: '20251010_071607'
+    up: migration_20251010_085528_initial.up,
+    down: migration_20251010_085528_initial.down,
+    name: '20251010_085528_initial'
   },
 ];

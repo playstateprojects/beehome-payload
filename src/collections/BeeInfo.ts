219462dc0787b13e9dfb1ae66d8350925f7a1baf
Tom <tom@playstate.co.za>
2025-12-14 22:54:48 +0100

    updates

--- src/collections/BeeInfo.ts
+++ src/collections/BeeInfo.ts
@@ -177,7 +177,6 @@ export const BeeInfo: CollectionConfig = {
             },
           ],
         },
-
         // ========================================
         // BODY SECTIONS TAB
         // ========================================
